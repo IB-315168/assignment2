6a15ee95ccea7b5f4685372df7197b1e5b50b482
Franciszek Nurkiewicz <318212@via.dk>
2022-10-31 22:30:33 +0100

    put on tray

--- src/test/testClient/src/main/java/com/sep3yg9/assignment2/testclient/TestClient.java
+++ src/test/testClient/src/main/java/com/sep3yg9/assignment2/testclient/TestClient.java
@@ -15,17 +15,32 @@ public class TestClient
                 .usePlaintext()
                 .build();
 
-        TrayServiceGrpc.TrayServiceBlockingStub stub = TrayServiceGrpc.newBlockingStub(channel);
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("type in weight");
-        double weight = Double.parseDouble(scanner.nextLine());
-        System.out.println("type in type");
-        String type = scanner.nextLine();
-        Tray tray = stub.createTray(Tray.newBuilder().setMaxWeight(weight).setFinished(false).setType(type).build());
-        System.out.println("tray "+tray );
-        System.out.println("Overview of trays :");
-        TrayList trayList = stub.getAllTrays(Empty.newBuilder().build());
-        System.out.println(trayList);
+        TrayServiceGrpc.TrayServiceBlockingStub trayStub = TrayServiceGrpc.newBlockingStub(channel);
+        PartServiceGrpc.PartServiceBlockingStub partStub = PartServiceGrpc.newBlockingStub(channel);
+
+        Part part1 = partStub.createPart(Part.newBuilder().setAnimalId(1).setWeight(1900).setType("TORSO").build());
+        System.out.println("Part created: " + part1);
+        part1 = partStub.createPart(Part.newBuilder().setAnimalId(1).setWeight(1000).setType("TORSO").build());
+        System.out.println("Part created: " + part1);
+        System.out.println("All parts: " + partStub.getAllRemainingParts(Empty.newBuilder().build()));
+
+        Tray tray1 = trayStub.createTray(Tray.newBuilder().setMaxWeight(3000).setType("TORSO").build());
+        System.out.println("Tray before: " + trayStub.getAllTrays(Empty.newBuilder().build()));
+
+        trayStub.putOnTray(PartTray.newBuilder().setPartId(1).setTrayId(1).build());
+        trayStub.putOnTray(PartTray.newBuilder().setPartId(2).setTrayId(1).build());
+
+        System.out.println("Tray after: " + trayStub.getAllTrays(Empty.newBuilder().build()));
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("type in weight");
+//        double weight = Double.parseDouble(scanner.nextLine());
+//        System.out.println("type in type");
+//        String type = scanner.nextLine();
+//        Tray tray = trayStub.createTray(Tray.newBuilder().setMaxWeight(weight).setFinished(false).setType(type).build());
+//        System.out.println("tray "+tray );
+//        System.out.println("Overview of trays :");
+//        TrayList trayList = trayStub.getAllTrays(Empty.newBuilder().build());
+//        System.out.println(trayList);
 
     }
 }
